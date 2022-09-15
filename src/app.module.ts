eec1b22a0375ddd13a5d51152a153a646c22feae
Software-Today <nicoli.dev.430@gmail.com>
2022-09-15 10:26:07 -0700

    readd-cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { UsersModule } from './users/users.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, CatsModule],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
